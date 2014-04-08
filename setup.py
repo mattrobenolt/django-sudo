7034d35cf0aacc41f1852da0f2925b03afe2ad41
Matt Robenolt <matt@ydekproductions.com>
2014-04-08 02:23:09 -0700

    0.0.3

--- setup.py
+++ setup.py
@@ -36,7 +36,7 @@ class PyTest(TestCommand):
 
 setup(
     name='django-sudo',
-    version='0.0.2',
+    version='0.0.3',
     author='Matt Robenolt',
     author_email='matt@ydekproductions.com',
     url='https://github.com/mattrobenolt/django-sudo',
