5eb655acf2e338d5250b6b2aa724ac0d168b309e
Armin Ronacher <armin.ronacher@active-4.com>
2016-05-24 02:16:30 +0200

    Adds class based view support for sudo.

--- sudo/settings.py
+++ sudo/settings.py
@@ -45,3 +45,6 @@ SUDO_FORM = getattr(settings, 'SUDO_FORM', 'sudo.forms.SudoForm')
 # The name of the session attribute used to preserve the redirect destination
 # between the original page request and successful sudo login.
 REDIRECT_TO_FIELD_NAME = getattr(settings, 'SUDO_REDIRECT_TO_FIELD_NAME', 'sudo_redirect_to')
+
+# The name of the view function for reversing
+SUDO_VIEW_NAME = getattr(settings, 'SUDO_VIEW_NAME', 'sudo.views.sudo')
