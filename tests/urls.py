1549c20cc6896997f1e21af1f22e3c2b923caad7
Matt Molyneaux <moggers87+git@moggers87.co.uk>
2017-01-08 00:45:35 +0000

    Run tests on Django 1.10

--- tests/urls.py
+++ tests/urls.py
@@ -1,10 +1,21 @@
-try:
-    from django.conf.urls import url, patterns
-except ImportError:
-    from django.conf.urls.defaults import url, patterns  # noqa
+import django
+
+from sudo import views
+
+if django.VERSION[:3] >= (1, 9, 0):
+    from django.conf.urls import url
+
+    urlpatterns = [
+        url(r'^sudo/', views.sudo, name='sudo'),
+    ]
+else:
+    try:
+        from django.conf.urls import url, patterns
+    except ImportError:
+        from django.conf.urls.defaults import url, patterns  # noqa
 
 
-urlpatterns = patterns(
-    '',
-    url(r'^sudo/', 'sudo.views.sudo', name='sudo'),
-)
+    urlpatterns = patterns(
+        '',
+        url(r'^sudo/', 'sudo.views.sudo', name='sudo'),
+    )
