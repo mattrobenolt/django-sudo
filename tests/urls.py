87b1a6fc6e0fafd2b020fbcbaa420c2fa1c4c61e
Matt Molyneaux <moggers87+git@moggers87.co.uk>
2017-01-08 01:13:13 +0000

    Make flake8 happy

--- tests/urls.py
+++ tests/urls.py
@@ -14,7 +14,6 @@ else:
     except ImportError:
         from django.conf.urls.defaults import url, patterns  # noqa
 
-
     urlpatterns = patterns(
         '',
         url(r'^sudo/', 'sudo.views.sudo', name='sudo'),
