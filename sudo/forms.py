6221d933d923a85ba039e814d9bb595f28cecedc
Joshua Li <joshuarli98@gmail.com>
2021-05-18 10:22:33 -0700

    test on 3.7, 3.8, update various things

--- sudo/forms.py
+++ sudo/forms.py
@@ -24,6 +24,11 @@ class SudoForm(forms.Form):
     def clean_password(self):
         username = self.user.get_username()
 
-        if auth.authenticate(request=None, username=username, password=self.data["password"]):
+        if auth.authenticate(
+            request=None,
+            username=username,
+            password=self.data["password"],
+        ):
             return self.data["password"]
+
         raise forms.ValidationError(_("Incorrect password"))
